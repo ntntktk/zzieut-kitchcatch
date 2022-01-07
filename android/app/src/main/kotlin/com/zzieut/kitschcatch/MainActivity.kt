da8e6727aeab3198249daf6166a98ad279c88372
careeringlabs <careeringlabs@gmail.com>
2022-01-07 18:18:37 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/zzieut/kitschcatch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zzieut.kitschcatch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ref/draw/main.dart
@@ -0,0 +1,14 @@
+import 'package:kitschcatch/ref/draw/draw_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(DrawApp());
+
+class DrawApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Draw(),
+    );
+  }
+}
